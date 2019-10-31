90b073d94f6232c67a5ff62ebf8f07d276b603e2
Kirito-1 <1487072880.com>
2019-10-31 06:58:20 -0700

    added a 123.c file

--- lab2/123.c
+++ lab2/123.c
@@ -1,26 +1,14 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(void) {
-    int a;
-    int b=0;
-    int c{100};
-    int count=0;
-    printf("输入一个四位数的正整数:\n");
-    scanf("%d",&a);
-    printf("二进制:");
-    while (a){
-        c[b]=a%2;
-        b++;
-        a/=2;
-
-    }
-    for(int j=b-1;j>=0;j--){
-        printf("%d",c[j]);
-        if(c[j]==1){
-    count++;
+#include<stdio.h>
+int main(){
+    long long input;
+    int lb=0;
+    scanf ("%lld",&input);
+    while(input){
+        if(input %2 == 1){
+            lb++;
         }
+        input/=2;
     }
-    printf("\n\"1\"出现的次数是%d\n",count);
-    system("pause");
+    printf("%d\n",lb);
     return 0;
 }
